28e839529b36eb2b6d33725dbd55f54d35d5c902
Johnson Mao <tutelary.maomao@gmail.com>
2022-11-21 12:00:51 +0800

    deploy

--- assets/NotFound.ad2e384d.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./index.2e98f9fe.js";import{o as e,d as r}from"./vue.d76b752f.js";const t={};function c(n,s){return e(),r("div",null,"\u4F60\u8FF7\u8DEF\u56C9")}const f=o(t,[["render",c]]);export{f as default};
